cf95b0a3f13f90103a618950153d23c702792464
Jerzy Balwinski <jurek_b@wp.pl>
2020-08-22 12:28:00 +0200

    Apka po sylizacji, przed zmiana przekierowania na 404 (zadanie)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { AppRoutingModule } from './app-routing.module';
+import { StudentEditComponent } from './student-edit/student-edit.component';
+import { FormsModule } from '@angular/forms';
+import { NotFoundComponent } from './not-found/not-found.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    StudentEditComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
